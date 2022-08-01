8a6bcd095be5219b1d8dd81181d835482de14bb5
Alayande Mustapha <Alayande Mustapha>
2022-08-01 21:46:07 +0100

    Moving on

--- parallel.java
+++ parallel.java
@@ -1,7 +1,18 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class parallel {
-public static void main(String[] arg){
-  System.out.println("Choose a number");
-  Scanner scanner = new Scanner(System.in)
+public static void main(String[] args){
+System.out.println("Pick a number between 1 and 10: ");
+Scanner scanner = new Scanner(System.in);
+
+int inputtedNum = scanner.nextInt();
+System.out.println(inputtedNum);
+if (inputtedNum < 5) {
+System.out.println("A good one, let's see");
+} 
+else {
+System.out.println("The number is also great");}
+/** else {System.out.println("Any choice is good");}
+} **/
+}
 }
