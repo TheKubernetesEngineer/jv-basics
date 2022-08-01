4ad15aae51165e0b2ce2a67010776e05d569d2e3
Alayande Mustapha <Alayande Mustapha>
2022-08-01 18:53:46 +0100

    Studnet class

---
+++ student.java
@@ -0,0 +1,18 @@
+public class student {
+public static void main(String[] args)
+{
+int studentAge = 15;
+double studentGPA = 3.45;
+boolean hasPerfectAttendance = true;
+String studentName = "Kayla Hammon";
+String studentFirstName = "Kayla";
+String studentLastName = "Hammon";
+char studentFirstInitial = studentFirstName.charAt(0);
+char studentLastInitial = studentLastName.charAt(0);
+System.out.println(studentAge);
+System.out.println(studentGPA);
+System.out.println(hasPerfectAttendance);
+System.out.println(studentName);
+System.out.println(studentFirstName + " " + studentLastName + " " + " has a GPA of " + studentGPA); 
+}
+}
