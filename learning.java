f1df2dc0b3d58eeac643d8a43e32359eeacef7fd
The Kubernetes Engineer <thekubernetesengineer@gmail.com>
2022-08-10 13:08:13 +0100

    Working with intelli j

---
+++ learning.java
@@ -0,0 +1,11 @@
+public class learning{
+public static void main(String[] args){
+    String student = "Learner";
+    char learner = student.charAt(0);
+    char learnerb = 'H';
+
+    System.out.println(learner);
+    System.out.println(learnerb);
+
+}
+}
